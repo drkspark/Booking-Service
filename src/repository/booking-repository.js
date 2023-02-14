8faadf8d41966811aaad2472aa5573597a22992b
drkspark <madhusudan753@gmail.com>
2023-02-14 22:07:59 +0530

    Added Booking Functionality

--- src/repository/booking-repository.js
+++ src/repository/booking-repository.js
@@ -1,22 +1,39 @@
-const { StatusCodes } = require('http-status-codes');
+const { StatusCodes } = require("http-status-codes");
 
-const { Booking } = require('../models/index');
-const { AppError, ValidationError } = require('../utils/index');
+const { Booking } = require("../models/index");
+const { AppError, ValidationError } = require("../utils/index");
 
 class BookingRepository {
     async create(data) {
         try {
             const booking = await Booking.create(data);
             return booking;
-        }
-        catch (error) {
-            if (error.name == 'SequelizeValidationError') {
+        } catch (error) {
+            if (error.name == "SequelizeValidationError") {
                 throw new ValidationError(error);
             }
-            throw new AppError (
-                'RepositoryError',
-                'Cannot create Booking',
-                'There was some issue creating the booking, please try agaain later',
+            throw new AppError(
+                "RepositoryError",
+                "Cannot create Booking",
+                "There was some issue creating the booking, please try agaain later",
+                StatusCodes.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    async update(bookingId, data) {
+        try {
+            const booking = await Booking.findByPk(bookingId);
+            if (data.status) {
+                booking.status = data.status;
+            }
+            await booking.save();
+            return booking;
+        } catch (error) {
+            throw new AppError(
+                "RepositoryError",
+                "Cannot update Booking",
+                "There was some issue updating the booking, please try again later",
                 StatusCodes.INTERNAL_SERVER_ERROR
             );
         }
